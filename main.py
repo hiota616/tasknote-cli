188ce9e16abcbb670d60af826466b97dd921d2bb
Alex <marssnikers1@mail.ru>
2026-07-07 16:41:48 +0300

    The structure of the future JSON has been created

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import uuid
+import json
+from datetime import datetime
+
 
 def meny():
     print('Select an action:')
@@ -8,16 +12,47 @@ def meny():
     print('0 - Close the program')
 
     try:
-        mode = input()
+        mode = int(input())
+        if mode == 1:
+            new_note = create_note()
+            write_file_note(new_note)
     except Exception as error:
         print(f'An error occurred: {error}')
     else:
         return mode
+    
 
-def create_note(note):
+def create_note():
+    try:
+        id = uuid.uuid4().hex
+        title = input('Enter a note title: ')
+        text_note = input('Enter the note text: ')
+        created_at =  datetime.now()
+        updated_at = None
+        deadline = input('Please specify a deadline: ')
+        status = 'in process'
+
+        notes = {
+            id: {
+                'title': title,
+                'text_note': text_note,
+                'created_at': created_at,
+                'updated_at': updated_at,
+                'deadline': deadline,
+                'status': status
+            }
+        }
+    except Exception as error:
+        print(f'An error occurred while creating the note: {error}')
+        return {}
+    else:
+        return notes
+
+
+
+def write_file_note(note):
     root = os.getcwd()
     note_file_path = os.path.join(root, 'notes.json')
-
     try:
         with open(note_file_path, 'a', encoding='utf-8') as note_file:
             note_file.write(note)
@@ -26,3 +61,9 @@ def create_note(note):
         print('You do not have permission to create a note.')
     except OSError:
         print('System error while creating a note')
+
+def main():
+    meny()
+
+
+main()    
