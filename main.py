6465db36157f15dcdd5ed5cdbabd667e7d8d2aa9
Alex <marssnikers1@mail.ru>
2026-07-08 12:21:30 +0300

    create note fix

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from datetime import datetime
 
 
 def meny():
-    while True:
-        print('Select an action:')
-        print('1 - Create a new note')
-        print('2 - Edit an existing note')
-        print('3 - Delete the note')
-        print('0 - Close the program')
+    print('Select an action:')
+    print('1 - Create a new note')
+    print('2 - View notes')
+    print('3 - Delete the note')
+    print('4 - Edit an existing note')
+    print('0 - Close the program')
 
-        try:
-            mode = int(input())
-        except Exception as error:
-            print(f'An error occurred: {error}')
-        else:
-            return mode
-        return None
+    try:
+        mode = int(input())
+    except Exception as error:
+        print(f'An error occurred: {error}')
+    else:
+        return mode
+    return None
         
 def get_data():
     root = os.getcwd()
@@ -38,31 +38,35 @@ def get_data():
 
 def create_note(notes):
     try:
-        id = uuid.uuid4().hex
+        note_id = uuid.uuid4().hex
         title = input('Enter a note title: ')
         text_note = input('Enter the note text: ')
         created_at =  str(datetime.now())
         updated_at = None
         deadline = input('Please specify a deadline: ')
-        status = 'in process'
+        status = 'in_process'
 
-        notes = {
-            id: {
+        notes[note_id] = {
                 'title': title,
                 'text_note': text_note,
                 'created_at': created_at,
                 'updated_at': updated_at,
                 'deadline': deadline,
                 'status': status
-            },
-        }
-    
+            }
+
     except Exception as error:
         print(f'An error occurred while creating the note: {error}')
-        return {}
     else:
         return notes
 
+
+def view_notes():
+    notes = get_data()
+    for key, values in notes.items():
+        print(f'{values['title']}')
+
+# def delete_note(notes, id):
 
 
 def write_file_note(note):
@@ -77,12 +81,19 @@ def write_file_note(note):
     except OSError:
         print('System error while creating a note')
 
-def main():
-    mode = meny()
-    if mode == 1:
-        notes_file = get_data()
-        note = create_note(notes_file)
-        write_file_note(note)
 
+def main():
+    while True:
+        mode = meny()
+        if mode == 1:
+            notes_file = get_data()
+            note = create_note(notes_file)
+            write_file_note(note)
+        elif mode == 2:
+            view_notes()
+        elif mode == 3:
+            pass
+        elif mode == 0:
+            break
 
 main()    
