fd573e86656e4e02046fbc8f96a71fccaaa8e5cb
Alex <marssnikers1@mail.ru>
2026-07-12 21:39:15 +0300

    Error handling has been added, along with some minor fixes

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_data():
             with open(note_file_path, 'r', encoding='utf-8') as json_file:
                 data = json.load(json_file)
                 if not isinstance(data, dict):
-                    return None
+                    raise ValueError('The notes file is corrupted. This is not a dictionary.')
                 
                 return data
 
@@ -29,8 +29,12 @@ def get_data():
             with open(note_file_path, 'w', encoding='utf-8') as json_file:
                 json.dump({}, json_file)
                 return {}
-
+            
+    except ValueError as error:
+        print(f'An error occurred during recording: {error}')
+        return None
     except json.decoder.JSONDecodeError:
+        print('The file is corrupted or empty.')
         return None
     except PermissionError:
         print('You do not have permission to view this note.')
@@ -43,14 +47,23 @@ def get_data():
 def write_file_note(note):
     root = os.getcwd()
     note_file_path = os.path.join(root, 'notes.json')
+    temp_file_path = os.path.join(root, 'temp_notes.json')
     try:
-        with open(note_file_path, 'w', encoding='utf-8') as note_file:
-            json.dump(note, note_file)
+        if not isinstance(note, dict):
+            raise ValueError("You're trying to save something other than a dictionary")
+        with open(temp_file_path, 'w', encoding='utf-8') as temp_file:
+            json.dump(note, temp_file, ensure_ascii=False, indent=4)
+
+        os.replace(temp_file_path, note_file_path)
     
+    except ValueError as error:
+        print(f'An error occurred during recording: {error}')
     except PermissionError:
         print('You do not have permission to create a note.')
     except OSError:
         print('System error while creating a note')
+    else:
+        print('The changes have been successfully made!')
 
 
 def create_note(notes):
@@ -59,7 +72,7 @@ def create_note(notes):
             note_id = uuid.uuid4().hex
             title = input('Enter a note title: ')
             text_note = input('Enter the note text: ')
-            if not title or not text_note:
+            if not title.strip() or not text_note.strip():
                 raise ValueError('Empty fields: Header or Text')
             created_at =  datetime.now().isoformat().split('T')
             updated_at = None
@@ -83,7 +96,7 @@ def create_note(notes):
         return None
     except ValueError as error:
         print(f'An error occurred while creating the note: {error}')
-        return notes
+        return None
     else:
         return notes
 
@@ -98,11 +111,13 @@ def print_note(key, values):
             print(f"Date and time of last edited: {values['updated_at'][0]} at {values['updated_at'][1]}")
         print(f"Deadline: {values['deadline']}")
         print(f"Status: {values['status']}")
-        if not values:
-            print('Empty.')
 
-    except IndexError:
+    except KeyError as error:
+        print(f'An error occurred while displaying the note: {error}')
+    except IndexError as error:
         print(f"The note {values['title']} was recorded incorrectly")
+    except TypeError as error:
+        print(f'An error occurred while displaying the note: {error}')
 
 
 def view_notes():
@@ -119,7 +134,9 @@ def view_notes():
                 ]
                 )
         for key, values in notes.items():
-            if view_mode == 'in_process':
+            if view_mode == 'return_to_main':
+                break
+            elif view_mode == 'in_process':
                 if values['status'] == view_mode:
                     print_note(key, values)
             elif view_mode == 'done':
@@ -130,18 +147,23 @@ def view_notes():
                     print_note(key, values)
             elif view_mode == 'all':
                 print_note(key, values)
-            elif view_mode == 'return_to_main':
-                break
+            
         print('-' * 40)
     else:
         print('Notes data was not loaded.')
                 
 
 def get_titles(notes):
-    titles = []
+    keys_titles = []
     for key, values in notes.items():
-        titles.append(values['title'])
-    return titles
+        try:
+            if not values['title']:
+                raise ValueError(f'No title. ID: {key}')
+            keys_titles.append((key, values['title']))
+        except ValueError as error:
+            print(f'An error occurred while creating the note: {error}')
+
+    return keys_titles
 
 
 def search_note_by_title(notes, title):
@@ -155,18 +177,37 @@ def search_note_by_title(notes, title):
 
 def delete_note():
     print('DELETE MENU')
-    notes = get_data()
-    if notes is not None:
-        titles = get_titles(notes)
-        titles.append({'name': 'Return to the main menu', 'value': 'return_to_main'})
-        title_note = menu('Select which note to delete:', titles)
-        if title_note != 'return_to_main':
-            note_id = search_note_by_title(notes, title_note)
-            notes.pop(note_id)
-            write_file_note(notes)
-            print(f'The note "{title_note}" has been deleted from the database.')
-    else:
-        print('Notes data was not loaded.')
+    try:
+        notes = get_data()
+
+        if notes is None:
+            raise ValueError('Empty fields: Notes')
+        
+        if notes == {}:
+            print('There are no notes in the database.')
+        else:
+            keys_titles = get_titles(notes)
+            print(keys_titles)
+            
+            keys_titles.append({'name': 'Return to the main menu', 'value': 'return_to_main'})
+            title_note = menu('Select which note to delete:', keys_titles)
+
+            if title_note != 'return_to_main':
+                note_id = search_note_by_title(notes, title_note)
+
+                if note_id is None:
+                    raise ValueError('Empty fields: id')
+                
+                clarification = menu('Are you sure you want to delete this note?', ['Yes', 'No'])
+
+                if clarification == 'Yes':
+                    notes.pop(note_id)
+                    write_file_note(notes)
+                
+            
+    except ValueError as error:
+        print(f'An error occurred while creating the note: {error}')
+
 
 def edit_note_value(notes, id_note, change_value, new_data):
     notes[id_note][change_value] = new_data
@@ -193,6 +234,8 @@ def edit_note():
                     )
 
             note_id = search_note_by_title(notes, title)
+            if note_id is None:
+                raise ValueError('Empty fields: id')
 
             if edit_mode != 'return_to_main':
             
